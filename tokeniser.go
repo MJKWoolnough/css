19025759407fcc276249580f82e83d8f5b9100db
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-22 16:29:14 +0000

    added stream preprocessing

--- tokeniser.go
+++ tokeniser.go
@@ -1,5 +1,11 @@
 package css
 
+import (
+	"io"
+
+	"vimagination.zapto.org/parser"
+)
+
 const (
 	whitespace   = " \t\r\n\f"
 	digits       = "0123456789"
@@ -7,3 +13,31 @@ const (
 	lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 	letters      = upperLetters + lowerLetters
 )
+
+type preprocessor struct {
+	*parser.Tokeniser
+}
+
+func (p *preprocessor) ReadRune() (rune, int, error) {
+	r := p.Next()
+	if r == -1 {
+		return 0, 0, io.EOF
+	}
+
+	switch r {
+	case '\r':
+		p.Accept("\n")
+
+		r = '\n'
+	case '\x0c':
+		r = '\n'
+	}
+
+	return r, 0, nil
+}
+
+func CreateTokeniser(t *parser.Tokeniser) *parser.Tokeniser {
+	tk := parser.NewRuneReaderTokeniser(&preprocessor{t})
+
+	return &tk
+}
