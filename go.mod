ae1f17b2b10c508c75816dca4677094bd73f8499
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-22 16:12:25 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module vimagination.zapto.org/css
+
+go 1.25.6
+
+require vimagination.zapto.org/parser v1.2.3
---
+++ tokeniser.go
@@ -0,0 +1,9 @@
+package css
+
+const (
+	whitespace   = " \t\r\n\f"
+	digits       = "0123456789"
+	upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+	letters      = upperLetters + lowerLetters
+)
