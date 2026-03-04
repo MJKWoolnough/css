0100d05e619b9295a2aba1c8a9af533c9c603382
Michael Woolnough <michael.woolnough@gmail.com>
2026-03-04 21:21:47 +0000

    added test numbers

--- utils_test.go
+++ utils_test.go
@@ -7,71 +7,71 @@ func TestUnquote(t *testing.T) {
 		Input, Output string
 		Err           error
 	}{
-		{
+		{ // 1
 			Input: ``,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 2
 			Input:  `""`,
 			Output: "",
 		},
-		{
+		{ // 3
 			Input:  `''`,
 			Output: "",
 		},
-		{
+		{ // 4
 			Input: `"`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 5
 			Input: `'`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 6
 			Input: `"'`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 7
 			Input: `''"`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 8
 			Input:  `"'"`,
 			Output: "'",
 		},
-		{
+		{ // 9
 			Input:  `'"'`,
 			Output: "\"",
 		},
-		{
+		{ // 10
 			Input: `'''`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 11
 			Input: `"""`,
 			Err:   ErrBadString,
 		},
-		{
+		{ // 12
 			Input:  `"abc"`,
 			Output: "abc",
 		},
-		{
+		{ // 13
 			Input:  `"\20"`,
 			Output: " ",
 		},
-		{
+		{ // 14
 			Input:  `"\41"`,
 			Output: "A",
 		},
-		{
+		{ // 15
 			Input:  `"\3D 123"`,
 			Output: "=123",
 		},
-		{
+		{ // 16
 			Input:  `"\= 123"`,
 			Output: "= 123",
 		},
-		{
+		{ // 17
 			Input:  `"\=\4D A"`,
 			Output: "=MA",
 		},
